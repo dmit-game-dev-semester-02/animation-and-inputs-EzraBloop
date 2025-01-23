8fdbf47aba8a58b8fa05f8efaf801f006b9bd840
EzraBloop <jpateytaylor1@nait.ca>
2025-01-23 10:42:14 -0700

    Initial set up

---
+++ Assignment01.cs
@@ -0,0 +1,47 @@
+﻿using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+using Microsoft.Xna.Framework.Input;
+
+namespace assignment01;
+
+public class Assignment01 : Game
+{
+    private GraphicsDeviceManager _graphics;
+    private SpriteBatch _spriteBatch;
+    private Texture2D _background, _box;
+    private CelAnimationSequence _sequence01, _sequence02;
+    private CelAnimationPlayer _animation01, animation02;
+    public Assignment01()
+    {
+        _graphics = new GraphicsDeviceManager(this);
+        Content.RootDirectory = "Content";
+        IsMouseVisible = true;
+    }
+
+    protected override void Initialize()
+    {
+
+        base.Initialize();
+    }
+
+    protected override void LoadContent()
+    {
+        _spriteBatch = new SpriteBatch(GraphicsDevice);
+
+    }
+
+    protected override void Update(GameTime gameTime)
+    {
+
+
+        base.Update(gameTime);
+    }
+
+    protected override void Draw(GameTime gameTime)
+    {
+        GraphicsDevice.Clear(Color.CornflowerBlue);
+
+
+        base.Draw(gameTime);
+    }
+}
